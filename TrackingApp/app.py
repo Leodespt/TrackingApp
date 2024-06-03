e52dab7d14851a68b1e9d3641f7ae24fb9a2b61a
Leodespt <112881176+Leodespt@users.noreply.github.com>
2024-06-03 13:06:00 +0200

    Delete TrackingApp directory

--- TrackingApp/app.py
+++
@@ -1,8 +0,0 @@
-# Dash Library
-import dash
-import dash_bootstrap_components as dbc
-
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.icons.FONT_AWESOME,dbc.icons.BOOTSTRAP])
-server = app.server
-app.config.suppress_callback_exceptions = True
