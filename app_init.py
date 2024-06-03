bb03bc143b5c6ebec1370172c6b7c1a26d2687df
leonard.desportes@gmail.com <leodespt@gmail.com>
2024-06-03 14:27:01 +0100

    c

--- app_init.py
+++
@@ -1,7 +0,0 @@
-# Dash Library
-import dash
-import dash_bootstrap_components as dbc
-
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.icons.FONT_AWESOME,dbc.icons.BOOTSTRAP])
-app.config.suppress_callback_exceptions = True
